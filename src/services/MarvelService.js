9ae7421c58e67e6d96eac7f1ddc115948b320515
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2021-12-17 15:25:24 +0300

    add api services

---
+++ src/services/MarvelService.js
@@ -0,0 +1,24 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=9f78f96162699cd12c0fde22c77d80f6';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Couldn't fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?${this._apiKey}`)
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    }
+}
+
+export default MarvelService;
